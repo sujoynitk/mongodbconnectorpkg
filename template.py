a618b0f52f191cc4b505433d5018d52f73403805
sujoynitk <sujoynitk@gmail.com>
2024-01-20 21:27:09 +0530

    folder structure created

---
+++ template.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path 
+
+list_of_files = [
+   ".github/workflows/.gitkeep",
+   "src/__init__.py",
+   "src/components/__init__.py", 
+   "src/components/data_ingestion.py", 
+   "src/components/data_transformation.py",
+   "src/components/model_trainer.py",
+   "src/components/model_evaluation.py",
+   "src/pipeline/__init__.py",
+   "src/pipeline/training_pipeline.py",
+   "src/pipeline/prediction_pipeline.py",
+   "src/utils/__init__.py",
+   "src/utils/utils.py",
+   "src/logger/__init__.py",
+   "src/logger/logger.py",
+   "src/exceptions/__init__.py",
+   "src/exceptions/exceptions.py",
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/integration/__init__.py",
+   "init_setup.sh",
+   "requirements.txt", 
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "experiments/experiments.ipynb"
+]
+
+for file in list_of_files:
+    file_path = Path(file)
+    file_dir, file_name = os.path.split(file_path)
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+    if ((not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0)):
+        with open(file_path, "w+") as f:
+            pass
